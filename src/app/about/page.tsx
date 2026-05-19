baa00843f7958c28e954dc0282d6a81f279bc44e
Kaviya M <kaviyamarimuthan005@gmail.com>
2026-05-19 15:51:18 +0530

    Portfolio initialized
    Portfolio initialized

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Frontend React Developer with 2 years of experience.</p>
+    </main>
+  );
+}
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,7 +1,6 @@
 
 export default function page() {
-    const skills :string[] =["aaa","sdsd","asas","sdsdS"];
-    const about :string="array"
+ 
   return (
     <div>page</div>
   )
